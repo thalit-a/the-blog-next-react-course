dbaaa66a5b8b6cecfa8e53d63ccfc0fe287e3c55
Thalita Ferreira <1998thalitaalves@gmail.com>
2025-07-21 18:33:38 -0300

    Admin Page Styling and Configuration.

---
+++ src/actions/post/delete-post-action.ts
@@ -0,0 +1,10 @@
+'use server';
+
+import { asyncDelay } from "@/utils/async-delay";
+import { logColor } from "@/utils/log-color";
+
+export async function deletePostAction(id: string) {
+  await asyncDelay(2000);
+  logColor('' + id);
+  return id;
+}
--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -1,13 +1,78 @@
 import { findAllPostAdmin } from "@/lib/post/queries/admin";
+import clsx from "clsx";
+import Link from "next/link";
+import { DeletePostButton } from "../admin/DeletePostButton";
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin()
 
   return (
-    <div className='py-16'>
+    <div className='mb-16'>
       {posts.map(post => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div
+            className={clsx(
+              'py-2 px-2',
+              !post.published && 'bg-slate-300',
+              'flex gap-2 items-center justify-between',
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+
+            {!post.published && (
+              <span className='text-xs text-slate-600 italic'>
+                (Não publicado)
+              </span>
+            )}
+
+            <DeletePostButton id={post.id} title={post.title} />
+          </div>
+        );
       })}
+
+      <div
+        className={clsx(
+          'fixed z-50 inset-0 bg-black/50 backdrop-blur-xs',
+          'flex items-center justify-center',
+        )}
+      >
+        <div
+          className={clsx(
+            'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
+            'flex flex-col gap-6',
+            'shadow-lg shadow-black/30 text-center',
+          )}
+        >
+          <h3 className='text-xl font-extrabold'>Título do diálogo</h3>
+          <p>
+            Lorem ipsum dolor sit amet. Eum dolor modi hic unde modi sed rerum dolores.
+            Ut quae impedit est doloribus nobis eos distinctio sequi?
+            Eum quas voluptas in nihil atque ut molestiae consequuntur eum inventore galisum aut obcaecati ipsum.
+          </p>
+          <div className='flex items-center justify-around'>
+            <button
+              className={clsx(
+                'bg-slate-300 hover:bg-slate-400 transition text-slate-950',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+              autoFocus
+            >
+              Cancelar
+            </button>
+            <button
+              className={clsx(
+                'bg-blue-500 hover:bg-blue-600 transition text-blue-50',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
